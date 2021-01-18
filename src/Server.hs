9f7ec35b4cb93413f05eb75f505441c72c4186ef
Hiroto Shioi <shioihigg@gmail.com>
2021-01-19 02:07:47 +0900

    Use rio env as custom handler monad

--- src/Server.hs
+++ src/Server.hs
@@ -1,16 +1,16 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE TypeOperators     #-}
-
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeOperators       #-}
 module Server where
 
 import           RIO
 
-import           Control.Monad.Logger                (runNoLoggingT)
-import qualified Data.ByteString.Lazy.Char8          as LC8
+import           Control.Monad.Logger        (runNoLoggingT)
+import qualified Data.ByteString.Lazy.Char8  as LC8
 import           Database.Persist.Postgresql
 import           Network.Wai.Handler.Warp
 import           Servant
@@ -50,44 +50,54 @@ type API auths =
 -- Handlers
 --------------------------------------------------------------------------------
 
-handleProtected :: Servant.Auth.Server.AuthResult Login -> ServerT ProtectedAPI App
+handleProtected :: Servant.Auth.Server.AuthResult Login -> ServerT ProtectedAPI (RIO Env)
 handleProtected (Authenticated userInfo) =
          return (getName $ loginUsername userInfo)
     :<|> return (loginEmail userInfo)
-handleProtected _ = throwAll err401
+handleProtected _ = throwM err401 :<|> throwM err401
 
 handleLogin
     :: LoginMessage
-    -> App (AcceptHeader NoContent)
+    -> RIO Env (AcceptHeader NoContent)
 handleLogin loginMessage = do
     Env{..} <- ask
     eUser <- liftIO $ runWithPool envDatabasePool $ loginUser loginMessage
     case eUser of
-        Left e -> throwError err401 { errBody = "Login error: " <> LC8.pack (show e) }
+        Left e -> throwM err401 { errBody = "Login error: " <> LC8.pack (show e) }
         Right userInfo -> do
             mApplyCookies <- liftIO $ acceptLogin envCookieSettings envJWTSettings userInfo
             case mApplyCookies of
-                Nothing           -> throwError err401
+                Nothing           -> throwM err401
                 Just applyCookies -> return $ applyCookies NoContent
 
-handleRegister :: RegisterMessage -> App Login
+handleRegister :: RegisterMessage -> RIO Env Login
 handleRegister registerMessage = do
     Env{..} <- ask
     eUserInfo <- liftIO $ runWithPool envDatabasePool $ registerUser registerMessage
     case eUserInfo of
-        Left e -> throwError err401 { errBody = "Register error: " <> LC8.pack (show e)}
+        Left e -> throwM $ err401 { errBody = "Register error: " <> LC8.pack (show e)}
         Right userInfo -> return userInfo
 
-server :: ServerT (API auths) App
+--------------------------------------------------------------------------------
+-- Server
+--------------------------------------------------------------------------------
+
+server :: ServerT (API auths) (RIO Env)
 server =
          handleProtected
     :<|> handleLogin
     :<|> handleRegister
 
-type App = ReaderT Env Servant.Handler
-
-nt :: Env -> App a -> Servant.Handler a
-nt ctx x = runReaderT x ctx
+nt :: forall a env. env -> RIO env a -> Servant.Handler a
+nt env x = transformation x
+  where
+    transformation :: RIO env a -> Servant.Handler a
+    transformation action = do
+        let ioAction = Right <$> runRIO env action
+        eitherRes <- liftIO $ ioAction `catch` \(e :: ServerError) -> pure $ Left e
+        case eitherRes of
+            Left servantErr -> throwError servantErr
+            Right res       -> pure res
 
 startServer :: IO ()
 startServer = do
@@ -101,8 +111,8 @@ startServer = do
       --- Here is the actual change
       api = Proxy :: Proxy (API '[JWT])
   let port = 7249
-  run port 
-    $ serveWithContext api cfg 
+  run port
+    $ serveWithContext api cfg
     $ hoistServerWithContext
         api
         (Proxy :: Proxy '[JWTSettings, CookieSettings])
